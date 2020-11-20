8eba50caa3eabbb937fc118734b7d5efad107f1f
Janos Buttgereit <io@schrammel.io>
2020-11-20 12:24:51 +0100

    Added license headers to all source files

--- Source/OJDParameters.cpp
+++ Source/OJDParameters.cpp
@@ -1,12 +1,23 @@
 /*
-  ==============================================================================
 
-    OJDParameters.cpp
-    Created: 14 Feb 2020 7:23:47pm
-    Author:  Janos Buttgereit
+This file is part of the Schrammel OJD audio plugin.
+Copyright (C) 2020  Janos Buttgereit
 
-  ==============================================================================
-*/
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+ */
 
 #include "OJDParameters.h"
 
